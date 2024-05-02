91f69a2125fcd408752949c99bc90ff05293a45a
Minami Doi <154774807+MinamiDoi@users.noreply.github.com>
2024-05-02 15:39:48 +0900

    exam-file

---
+++ _src/js/index.js
@@ -0,0 +1,9 @@
+
+// $(document).ready(function(){
+//     $.ajax({url:'https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&hourly=precipitation_probability,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=Asia%2FTokyo&forecast_hours=6'})
+//     .done(function(date){
+
+
+//     });
+// });
+
